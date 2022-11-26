b6e85ba6678225903bd9e8f6787ddadf1f861fda
ghartemann <g.hartemann@gmail.com>
2022-11-26 08:34:37 +0100

    save

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -1,59 +1,52 @@
 <template>
-    <v-container class="d-flex flex-column justify-center test">
-        <v-row class="d-flex flex-column justify-center">
-            <v-col class="d-flex">
-                <h1 class="align-self-end white--text">Regarderou</h1>
-            </v-col>
-            <v-col>
-                <v-autocomplete
-                    @update:search="search($event)"
-                    v-model="input"
-                    :items="results.results"
-                    :loading="loading"
-                    :placeholder="this.placeholder"
-                    clearable
-                    hide-selected
-                    autofocus
-                    color="white"
-                    hide-no-data
-                    variant="outlined"
-                >
-                </v-autocomplete>
-            </v-col>
-        </v-row>
+    <v-container
+        fluid class="d-flex flex-column justify-center test"
+        :style="providers && results.results && input && input !== '' ? 'background-image:' + background + 'background-size: cover; width: 100vw;' : ''">
 
-        <v-row v-if="providers && results.results && input !== null" justify="space-around">
-            <v-card  width="1000">
-                <v-img
-                    height="500px"
-                    :src="providers && results.results ? 'https://www.themoviedb.org/t/p/original/' + results.results[0].backdrop_path : 'https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg'"
-                >
-
-                    <v-card-title class="white--text mt-8 d-flex align-center">
-                        <v-avatar size="56">
-                            <img
-                                alt="user"
-                                :src="providers && results.results ? 'https://www.themoviedb.org/t/p/original/' + results.results[0].poster_path : 'https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg'"
-                            >
-                        </v-avatar>
-                        <p class="ml-3">
-                            {{ providers && results.results ? results.results[0].original_title : "Rien" }}
-                        </p>
-                    </v-card-title>
-                </v-img>
-
-                <v-card-text>
-                    <div v-if="providers && providers.results && providers.results.FR" v-for="provider in providers.results.FR.flatrate">
-                        <h2>Disponible sur :</h2>
-                        <div class="d-flex">
-                            <img :src="'https://www.themoviedb.org/t/p/original/' + provider.logo_path" :alt="provider.provider_name" class="rounded-lg mr-2" width="64">
-                            <div>{{ provider.provider_name }}</div>
-                        </div>
+        <transition-group name="list" mode="out-in">
+            <v-row key="1">
+                <v-spacer></v-spacer>
+                <v-col class="d-flex flex-column justify-center col-6">
+                    <div>
+                        <v-autocomplete
+                            @update:search="search($event)"
+                            v-model="input"
+                            :items="results.results"
+                            :loading="loading"
+                            :placeholder="placeholder"
+                            clearable
+                            hide-selected
+                            autofocus
+                            color="white"
+                            hide-no-data
+                            return-object
+                            rounded
+                            variant="solo"
+                            :menu-props="{ maxHeight: 200 }"
+                        >
+                        </v-autocomplete>
                     </div>
-                    <div v-if="providers && providers.results && providers.results.FR && !providers.results.FR.flatrate">Ce film n'est pas disponible en streaming.</div>
-                </v-card-text>
-            </v-card>
-        </v-row>
+                </v-col>
+                <v-spacer></v-spacer>
+            </v-row>
+    
+            <v-row key="2" v-if="providers && results.results && input !== null" justify="space-around">
+                <v-spacer></v-spacer>
+                <v-col class="col-6">
+                    <v-card>
+                        <v-card-text>
+                            <div v-if="providers && providers.results && providers.results.FR" v-for="provider in providers.results.FR.flatrate">
+                                <div class="d-flex">
+                                    <img :src="'https://www.themoviedb.org/t/p/original/' + provider.logo_path" :alt="provider.provider_name" class="rounded-lg mr-2" width="64">
+                                </div>
+                            </div>
+                            <div v-if="providers && providers.results && providers.results.FR && !providers.results.FR.flatrate">Ce film n'est pas disponible en streaming.</div>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-spacer></v-spacer>
+            </v-row>
+        </transition-group>
     </v-container>
 
 </template>
@@ -75,7 +68,9 @@ export default {
             searchTimeout: null,
             trending: [],
             placeholder: '',
+            placeholderIndex: null,
             loadingTrending: true,
+            background: "none"
         }
     },
     methods: {
@@ -92,6 +87,8 @@ export default {
                         .then((r) => {
                             this.results = r.data;
                             this.movieId = this.results.results[0].id;
+
+                            this.background = "url('https://www.themoviedb.org/t/p/original" + this.results.results[0].backdrop_path + "');";
                             this.fetch();
                         }).catch((error) => {
                             console.error(error);
@@ -128,6 +125,7 @@ export default {
                         this.trending.push(r.data.results[i].title);
                     }
                     this.getRandomPlaceholder();
+                    this.autoChangePlaceholder();
                 }).catch((error) => {
                     console.error(error);
             }).finally(() => {
@@ -136,6 +134,19 @@ export default {
         },
         getRandomPlaceholder() {
             this.placeholder = this.trending[Math.floor(Math.random() * (this.trending.length))];
+            this.placeholderIndex = this.trending.findIndex((x) => x === this.placeholder);
+        },
+        autoChangePlaceholder() {
+            setInterval(this.changePlaceholder, 1500);
+        },
+        changePlaceholder() {
+            this.placeholderIndex++;
+
+            if (this.placeholderIndex === this.trending.length) {
+                this.placeholderIndex = 0;
+            }
+
+            this.placeholder = this.trending[this.placeholderIndex];
         }
     },
     created() {
@@ -151,5 +162,23 @@ export default {
 
 .test {
     min-height: 100vh;
+}
+
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+    transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+    position: absolute;
 }
 </style>
